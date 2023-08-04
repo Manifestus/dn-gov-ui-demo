549e04cda4dec70d51a9a47de2ce049fa74a2860
Manifestus <link_figueroa20@hotmail.com>
2023-08-03 22:21:03 -0600

    Changes on auth config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const auth0Config = {
   //localhost
-  base_url: "http://localhost:3001",
-  client_id: "67D2W3AO6ivipSdhQzvqAlWtdmhA54zR",
-  issuer_base_url: "sv-perulapia.us.auth0.com",
-  
-  // base_url: "https://delnorte-github-ui-5ywbgrw4ia-uc.a.run.app",
-  // client_id: "D27aAV3OvCAjaM0qg4ZlDVUnbDjwfze9",
+  // base_url: "http://localhost:3001",
+  // client_id: "67D2W3AO6ivipSdhQzvqAlWtdmhA54zR",
   // issuer_base_url: "sv-perulapia.us.auth0.com",
+  
+  base_url: "https://dn-gh-ui-demopage-5ywbgrw4ia-ue.a.run.app",
+  client_id: "ldA5C73ViwUIyPUZlX65qNy4wZ5B2cjY",
+  issuer_base_url: "sv-perulapia.us.auth0.com",
 };
